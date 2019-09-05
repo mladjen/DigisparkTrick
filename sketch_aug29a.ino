0533c2d8b1dc02f86e6cd6a77770bd2a092511a2
mladjen <mladjen12@gmail.com>
2019-09-06 00:02:26 +1000

    initial commit

---
+++ sketch_aug29a.ino
@@ -0,0 +1,12 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+    DigiKeyboard.sendKeyStroke(0,MOD_GUI_LEFT); // Windows key
+    DigiKeyboard.delay(1500);                   // wait 1.5 seconds
+    DigiKeyboard.println("cmd");                // searches for cmd and presses enter
+    DigiKeyboard.delay(1000);                   // waits 1 second for cmd to open
+    DigiKeyboard.println("cd Desktop");         // change directory to desktop
+    DigiKeyboard.println("ECHO dont leave your lappy unlocked silly  your lappy unlocked silly ;P > clickme.txt"); // your message here
+}
+void loop() {
+}
